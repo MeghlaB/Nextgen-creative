719430a9dba35c6208c13425dfd4f2415a293c10
MeghlaB <meghlab964@gmail.com>
2025-06-22 21:24:44 +0600

    navbar fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,15 +6,10 @@ const pageTitleFirst = typeof document != "undefined" ? document?.title : siteDa
 const Navbar = ({siteData}) => {
     const location = useLocation();
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const [isHidden, setIsHidden] = useState(false);
     const [isTopScrollY, setIsTopScrollY] = useState(location.pathname == "/");
     const [pageTitle, setPageTitle] = useState(pageTitleFirst);
 
-
-
-    let lastScrollTop = 0;
     useEffect(() => {
-
         if(location.pathname == "/"){
             document.title = pageTitleFirst;
         }else{
@@ -32,39 +27,30 @@ const Navbar = ({siteData}) => {
                 })){
                     location.hash == "" && window.scrollTo({top: 0,left: 0,behavior: "smooth",});
                 }
-                
             }
         });
 
         setIsTopScrollY(location.pathname == "/" && (window.pageYOffset || document.documentElement.scrollTop) <= 100);
         const handleScroll = () => {
             let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-            
-            setIsHidden(currentScroll > lastScrollTop && (document.body.offsetHeight - currentScroll) > (window.innerHeight + 300));
-            setIsMenuOpen(false);
             setIsTopScrollY(location.pathname == "/" && currentScroll <= 100);
-
-            lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
         };
 
         window.addEventListener("scroll", handleScroll);
         return () => window.removeEventListener("scroll", handleScroll);
     }, [location]);
 
-
     useEffect(() => {
         document.title = pageTitle;
     }, [pageTitle]);
-
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
     };
 
-
     return (
-        <nav className={`navbarTop ${isHidden && "translate-y-[-100%]"} ${isTopScrollY && "!bg-transparent !shadow-none py-4"}`}>
-            <div className="w-10/12 mx-auto px-6 py-4 flex justify-between items-center">
+        <nav className={`fixed top-0 left-0 w-full z-50 transition-all duration-300 ${isTopScrollY ? "!bg-transparent !shadow-none py-4" : "bg-white shadow-md py-2"}`}>
+            <div className="w-10/12 mx-auto px-6 flex justify-between items-center">
                 {/* Logo */}
                 <Link to="/" className="text-2xl font-bold text-gray-800 w-[150px]">
                     <img src={siteData.siteLogo} className={`transition-all w-[${isTopScrollY ? 150 : 120}px]`} />
@@ -81,23 +67,8 @@ const Navbar = ({siteData}) => {
 
                 {/* Navigation Links */}
                 <ul
-                    className={`${!isMenuOpen && "max-lg:translate-y-[-200%]"} transition-all lg:flex lg:items-center lg:space-x-8 bg-white lg:bg-transparent absolute lg:static w-full lg:w-auto left-0 top-[100%] lg:mt-0 p-4 lg:p-0`}
+                    className={`${!isMenuOpen ? "max-lg:translate-y-[-200%]" : "max-lg:translate-y-0"} transition-all lg:flex lg:items-center lg:space-x-8 bg-white lg:bg-transparent absolute lg:static w-full lg:w-auto left-0 top-[100%] lg:top-auto lg:mt-0 p-4 lg:p-0 shadow-lg lg:shadow-none`}
                 >
-                    {/* <li>
-                        <Link to="/" className="navbar-item">
-                            Overview
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/#why-choose-us" className="navbar-item">
-                            Why We?
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/#reviews" className="navbar-item">
-                            Reviews
-                        </Link>
-                    </li> */}
                     <li>
                         <Link to="/about" className="navbar-item">
                             About
